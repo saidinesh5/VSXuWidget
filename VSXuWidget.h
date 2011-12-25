c85c860fdbb5a89c3585d5768680c589347b3484
Dinesh Manajipet <saidinesh5@gmail.com>
2011-12-26 03:56:13 +0530

    The Initial commit for a perfectly compiling VSXuWidget

--- VSXuWidget.h
+++ VSXuWidget.h
@@ -1,14 +1,34 @@
 #ifndef VSXuWidget_H
 #define VSXuWidget_H
 
-#include <QtGui/QMainWindow>
+#include <vsx_manager.h>
+#include <QGLWidget>
+#include <QTimer>
+#include <QStringList>
 
-class VSXuWidget : public QMainWindow
+class VSXuWidget : public QGLWidget
 {
-Q_OBJECT
+  Q_OBJECT
+private:
+  vsx_manager_abs* m_manager;
+  QTimer *m_timer; // A Timer to frequently update the display
+  
+  QStringList m_visuals;
+  int m_width,m_height;
+  bool m_isActive; // To Save the CPU when we aren't using VSXu.
+
+protected:
+  void initializeGL();
+  void resizeGL(int w, int h){
+    m_width = w;
+    m_height = h;
+  }
+  void paintGL();
+  
 public:
-    VSXuWidget();
-    virtual ~VSXuWidget();
+  VSXuWidget(QWidget *parent = NULL);
+  ~VSXuWidget();
+  void injectSound(float soundData[]);
 };
 
 #endif // VSXuWidget_H
